abf31ca756c74d9d53a99f613f76c2a11451b05a
kunalkale98 <kunal,k2898@gmail.com>
2021-02-26 09:59:58 +0530

    Added: Create a board

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,5 +1,15 @@
 class TicTacToe{
+
+	public char[] createBoard(){
+		char[] board = new char[10];
+		for(int i=0; i<=9; i++){
+			board[i] = ' ';
+		}
+		return board;
+	}
+
 	public static void main(String args[]){
-		System.out.println("Welcome to TicTacToe Game");
+		TicTacToe obj = new TicTacToe();
+		obj.createBoard();
 	}
 }
