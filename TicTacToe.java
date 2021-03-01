ab620c1cdc1522b5896721e57af89a82f15af509
kunalkale98 <kunal,k2898@gmail.com>
2021-03-02 03:17:28 +0530

    Added: Toss to see who plays first

--- TicTacToe.java
+++ TicTacToe.java
@@ -18,6 +18,7 @@ class TicTacToe{
 
 	public void showBoard(char userLetter,char compLetter,char[] board,int pos){
 		board[pos]=userLetter;
+		board[1]=compLetter;
 		System.out.println(" "+board[1]+" | "+board[2]+" | "+board[3]+" ");
 		System.out.println("-----------");
 		System.out.println(" "+board[4]+" | "+board[5]+" | "+board[6]+" ");
@@ -26,75 +27,86 @@ class TicTacToe{
 	}
 
 	public int userMove(Scanner sc, char[] board){
-		System.out.println("Enter the Position:");
+		System.out.print("Enter the Position: ");
 		int choice = sc.nextInt();
 		switch(choice){
-			case 1:
-				if(board[1] == ' '){
-					return 1;
-				}
-				else{
-					System.out.println("Place is not empty");
-				}
-			case 2:
-				if(board[2] == ' '){
-					return 2;
-				}
-				else{
-					System.out.println("Place is not empty");
-				}
-			case 3:
-				if(board[3] == ' '){
-					return 3;
-				}
-				else{
-					System.out.println("Place is not empty");
-				}
-			case 4:
-				if(board[4] == ' '){
-					return 4;
-				}
-				else{
-               System.out.println("Place is not empty");
-            }
-		 	case 5:
-				if(board[5] == ' '){
-					return 5;
-				}
-				else{
-					System.out.println("Place is not empty");
-				}
-			case 6:
-				if(board[6] == ' '){
-					return 6;
-				}
-				else{
-					System.out.println("Place is not empty");
-				}
-			case 7:
-				if(board[7] == ' '){
-					return 7;
-				}
-				else{
-					System.out.println("Place is not empty");
-				}
-			case 8:
-				if(board[8] == ' '){
-					return 8;
-				}
-				else{
-					System.out.println("Place is not empty");
-				}
-			case 9:
-				if(board[9] == ' '){
-					return 9;
-				}
-				else{
-					System.out.println("Place is not empty");
-				}
-			default:
-				System.out.println("Enter posititon between 1-9");
-				return userMove(sc,board);
+		case 1:
+			if(board[1] == ' '){
+				return 1;
+			}
+			else{
+				System.out.println("Place is not empty");
+			}
+		case 2:
+			if(board[2] == ' '){
+				return 2;
+			}
+			else{
+				System.out.println("Place is not empty");
+			}
+		case 3:
+			if(board[3] == ' '){
+				return 3;
+			}
+			else{
+				System.out.println("Place is not empty");
+			}
+		case 4:
+			if(board[4] == ' '){
+				return 4;
+			}
+			else{
+				System.out.println("Place is not empty");
+			}
+		case 5:
+			if(board[5] == ' '){
+				return 5;
+			}
+			else{
+				System.out.println("Place is not empty");
+			}
+		case 6:
+			if(board[6] == ' '){
+				return 6;
+			}
+			else{
+				System.out.println("Place is not empty");
+			}
+		case 7:
+			if(board[7] == ' '){
+				return 7;
+			}
+			else{
+				System.out.println("Place is not empty");
+			}
+		case 8:
+			if(board[8] == ' '){
+				return 8;
+			}
+			else{
+				System.out.println("Place is not empty");
+			}
+		case 9:
+			if(board[9] == ' '){
+				return 9;
+			}
+			else{
+				System.out.println("Place is not empty");
+			}
+		default:
+			System.out.println("Enter posititon between 1-9");
+			return userMove(sc,board);
+		}
+	}
+
+	public int toss(Scanner sc){
+		int isHead = 1;
+		int coin = (int)Math.floor((Math.random()*10)%2);
+		if (coin == isHead){
+			return 1;
+		}
+		else{
+			return 0;
 		}
 	}
 
@@ -102,10 +114,21 @@ class TicTacToe{
 		Scanner sc = new Scanner(System.in);
 		char[] board = new char[10];
 		TicTacToe obj = new TicTacToe();
+		char userLetter, compLetter;
 		board = obj.createBoard();
-		char userLetter = obj.chooseInput(sc);
-		char compLetter = (userLetter=='X')?'O':'X';
+		int toss = obj.toss(sc);
+		if(toss == 1){
+			userLetter = obj.chooseInput(sc);
+			compLetter = (userLetter=='X')?'O':'X';
+		}
+		else{
+			compLetter = 'X';
+			userLetter = 'O';
+			System.out.println("Computer played with X, you are O");
+
+		}
 		int pos = obj.userMove(sc,board);
 		obj.showBoard(userLetter,compLetter,board,pos);
 	}
 }
+
